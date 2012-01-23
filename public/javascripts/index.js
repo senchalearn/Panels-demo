2e5e26af6b03f7be3dfef9a999a5eaf2421a0864
Drew Neil <andrew.jr.neil@gmail.com>
2012-01-23 16:06:55 +0100

    Switch from onReady() to application launch()

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,45 +1,48 @@
-Ext.setup({
-  onReady: function() {
+Ext.Loader.setConfig({enabled: true});
 
-    var dimensions = [340,210,130,80,50,30,20,10];
-    
-    new Ext.Panel({
-      fullscreen: true,
-      items: [{
-        width: dimensions[0],
-        height: dimensions[1],
-        cls: 'panel-1',
-        items: [{
-          docked: 'right',
-          width: dimensions[2],
-          height: dimensions[1],
-          cls: 'panel-2',
-          items: [{
-            docked: 'top',
-            width: dimensions[2],
-            height: dimensions[3],
-            cls: 'panel-3',
+Ext.application({
+    name: 'App',
+
+    launch: function() {
+        var dimensions = [340,210,130,80,50,30,20,10];
+
+        Ext.Viewport.add({
+            fullscreen: true,
             items: [{
-              docked: 'left',
-              width: dimensions[4],
-              height: dimensions[3],
-              cls: 'panel-4',
-              items: [{
-                docked: 'bottom',
-                width: dimensions[4],
-                height: dimensions[5],
-                cls: 'panel-5',
+                width: dimensions[0],
+                height: dimensions[1],
+                cls: 'panel-1',
                 items: [{
-                  docked: 'right',
-                  width: dimensions[6],
-                  height: dimensions[5],
-                  cls: 'panel-6'
+                    docked: 'right',
+                    width: dimensions[2],
+                    height: dimensions[1],
+                    cls: 'panel-2',
+                    items: [{
+                        docked: 'top',
+                        width: dimensions[2],
+                        height: dimensions[3],
+                        cls: 'panel-3',
+                        items: [{
+                            docked: 'left',
+                            width: dimensions[4],
+                            height: dimensions[3],
+                            cls: 'panel-4',
+                            items: [{
+                                docked: 'bottom',
+                                width: dimensions[4],
+                                height: dimensions[5],
+                                cls: 'panel-5',
+                                items: [{
+                                    docked: 'right',
+                                    width: dimensions[6],
+                                    height: dimensions[5],
+                                    cls: 'panel-6'
+                                }]
+                            }]
+                        }]
+                    }]
                 }]
-              }]
             }]
-          }]
-        }]
-      }]
-    });
-  }
+        });
+    }
 });
